67b6c0bee6ef6aba668efad6ab553892b9a10795
Oskar Domingos <oskarad.2000@gmail.com>
2022-03-29 17:20:26 +0100

    New code added

--- src/main/java/Server/ConnectionHandler.java
+++ src/main/java/Server/ConnectionHandler.java
@@ -3,8 +3,9 @@ package Server;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.Collection;
 import java.util.Scanner;
 
 // New thread that handles the communication between server and one client
@@ -12,23 +13,26 @@ import java.util.Scanner;
 class ConnectionHandler extends Thread {
     private Socket socket;
     private Scanner in;
-    private PrintWriter out;
+    private ObjectOutputStream out;
 
     public ConnectionHandler(Socket socket) {
         this.socket = socket;
     }
 
     public void run() {
+        SharedState sharedState = SharedState.getInstance();
+
+        // TODO: Spawn Separate thread for checking alive message
+
         try {
             // Create input stream and output stream
             in = new Scanner(socket.getInputStream());
-            out = new PrintWriter(socket.getOutputStream(), true);
+            out = new ObjectOutputStream(socket.getOutputStream());
 
-            SharedState sharedState = SharedState.getInstance();
-
+            // Firstly, gather info about client
             while (true) {
                 // Send info to the client that server is waiting for the information about client
-                out.println(Headers.CLIENT_INFO_AWAITING);
+                out.writeChars(Headers.CLIENT_INFO_AWAITING.toString());
                 // Accept line from the connection
                 String response = in.nextLine();
 
@@ -38,33 +42,60 @@ class ConnectionHandler extends Thread {
                     String clientInfo = in.nextLine();
                     if (sharedState.isClientInfoValid(clientInfo)) {
                         // If client data are valid, send header informing about that
-                        out.println(Headers.CLIENT_INFO_VALID.toString());
-
-                        sharedState.addMember(in.nextLine(), this.getId());
+                        out.writeChars(Headers.CLIENT_INFO_VALID.toString());
+                        sharedState.addMember(in.nextLine(), this.getId(), socket.getInetAddress().toString());
                     } else {
-                        out.println(Headers.CLIENT_INFO_INVALID.toString());
+                        out.writeChars(Headers.CLIENT_INFO_INVALID.toString());
+
+                        // Close the connection and terminate the thread
+                        sharedState.removeMember(this.getId());
+                        break;
                     }
+                } else {
+                    // If header is different than CLIENT_INFO_SENDING, send message to client that invalid data was sent
+                    out.writeChars(Headers.CLIENT_INFO_INVALID.toString());
+
+                    // Close the connection and terminate the thread
+                    sharedState.removeMember(this.getId());
                     break;
                 }
             }
 
-            while(true) {
+            // Then wait for other information like message or disconnect ??
+            while(!this.isInterrupted()) {
                 String response = in.nextLine();
 
                 if (response.equals(Headers.MESSAGE.toString())) {
                     response = in.nextLine();
                     sharedState.addMessage(response, this.getId());
+                } else {
+                    out.writeChars(Headers.INVALID_HEADER.toString());
                 }
             }
 
-            // TODO: finish that part
+        } catch (IOException exception) {
+            // In case of error remove user from members map
+            sharedState.removeMember(this.getId());
+            exception.printStackTrace();
+        }
+    }
+
+
+    public void sendMessage(@NotNull Headers messageHeader, String messageContent) {
+        try {
+            out.writeChars(messageHeader.toString());
+            out.writeChars(messageContent);
         } catch (IOException exception) {
             exception.printStackTrace();
         }
     }
 
-    public void sendMessage(@NotNull Headers messageHeader, String messageContent) {
-        out.write(messageHeader.toString());
-        out.write(messageContent);
+    public void sendMembersList(@NotNull Headers messageHeader, Collection<Member> members) {
+        try {
+            out.writeChars(messageHeader.toString());
+            out.writeObject(members);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
     }
 }
