cad12e39ad9534790efb5951f1e63dddb7fea848
Oskar Domingos <oskarad.2000@gmail.com>
2022-03-29 18:38:46 +0100

    Merge main to server

--- src/main/java/Server/Headers.java
+++ src/main/java/Server/Headers.java
@@ -4,8 +4,11 @@ public enum Headers {
     MESSAGE, // Informs the server that next lines will contain the message
     MESSAGE_RECEIVED, // Informs the client that message was received
     CLIENT_INFO_AWAITING, // informs the connection that server is waiting for the username and unique id
-    CLIENT_INFO_VALID, // informs the client that username and unique id is valid
     CLIENT_INFO_SENDING, // informs the server that next line will contain the information about client
+    CLIENT_INFO_VALID, // informs the client that username and unique id is valid
     CLIENT_INFO_INVALID, // Informs the client that the client info was invalid
-    USERS_LIST // Informs the client that the next line contains list of all users connected
+    USERS_LIST, // Informs the client that the next line contains list of all users connected
+    INVALID_HEADER,
+    COORDINATOR_INFO, // Informs Client that user is a coordinator
+    CLIENT_ALIVE, // Informs server that client is still alive and should not be removed
 }
