851a270b913a927a721acbd2e6f7565b8d2b7658
Oskar Domingos <oskarad.2000@gmail.com>
2022-03-31 00:31:33 +0100

    More tests added.

--- src/main/java/Server/SharedStateTest.java
+++ src/main/java/Server/SharedStateTest.java
@@ -18,6 +18,7 @@ import java.util.List;
 @RunWith(MockitoJUnitRunner.class)
 class SharedStateTest {
 
+    private SharedState sharedState;
     private ConnectionHandler connectionHandler1;
     private ConnectionHandler connectionHandler2;
     private long thread1;
@@ -25,12 +26,14 @@ class SharedStateTest {
 
     @BeforeEach
     public void setUp() {
+        sharedState = SharedState.getInstance();
         thread1 = 1234567;
         thread2 = 1245678;
         connectionHandler1 = mock(ConnectionHandler.class);
         connectionHandler2 = mock(ConnectionHandler.class);
         when(connectionHandler1.getId()).thenReturn(thread1);
         when(connectionHandler2.getId()).thenReturn(thread2);
+
     }
 
     @AfterEach
@@ -52,7 +55,6 @@ class SharedStateTest {
     void initiateMember() {
         // Initiate member should create new instance of the Member
         // and add it to the Hash map, but not to LinkedList
-        SharedState sharedState = SharedState.getInstance();
 
         HashMap<Long, Member> membersMap = sharedState.getMembersMap();
         LinkedList<String> membersOrder = sharedState.getMembersOrder();
@@ -73,8 +75,6 @@ class SharedStateTest {
     @Test
     void addMessage() {
         // Check that addMessage adds message to the list and is being sent to the user
-        SharedState sharedState = SharedState.getInstance();
-
         String clientId = "123ab";
         String username = "oskar";
         String ipAddress = "127.0.0.1";
@@ -95,14 +95,14 @@ class SharedStateTest {
 
     @Test
     void addMember() {
-        SharedState sharedState = SharedState.getInstance();
-
         LinkedList<String> membersOrder = sharedState.getMembersOrder();
         HashMap<Long, Member> membersMap = sharedState.getMembersMap();
 
+        // Before adding a user, membersOrder list and membersMap map should be empty
         assertEquals(0, membersOrder.size());
         assertEquals(0, membersMap.size());
 
+        // Create a member
         sharedState.initiateMember(connectionHandler1);
 
         String userId = "123ab";
@@ -114,23 +114,26 @@ class SharedStateTest {
         membersMap = sharedState.getMembersMap();
 
         Member createdMember = membersMap.get(thread1);
+        // Newly created member should be coordinator, because he is the first
+        assertTrue(createdMember.getCoordinator());
 
+        // membersMap and membersOrder should have size equal to 1
         assertEquals(1, membersMap.size());
         assertEquals(1, membersOrder.size());
 
+        // Check if correct info is added to members order
         assertEquals(userId, membersOrder.getFirst());
 
+        // Check if correct info is added to members map
         assertEquals(userId, createdMember.getId());
         assertEquals(username, createdMember.getUsername());
         assertEquals(ipAddress, createdMember.getIpAddress());
-
     }
 
     @Test
     void isClientInfoValid() {
         String clientInfo = "123ab:oskar";
 
-        SharedState sharedState = SharedState.getInstance();
         boolean clientInfoValid = sharedState.isClientInfoValid(clientInfo);
 
         assertTrue(clientInfoValid);
@@ -144,9 +147,47 @@ class SharedStateTest {
 
     @Test
     void removeAllMembers() {
+        // Firstly add some members
+        sharedState.initiateMember(connectionHandler1);
+        sharedState.initiateMember(connectionHandler2);
+        sharedState.addMember("123ab:oskar", thread1, "127.0.0.1");
+        sharedState.addMember("124ab:pedro", thread2, "127.0.0.1");
+
+        // There should be two members in a SharedState
+        assertEquals(2, sharedState.getMembersOrder().size());
+        assertEquals(2, sharedState.getMembersMap().size());
+
+        sharedState.removeAllMembers();
+
+        // After clearing there should not be any member in a SharedState
+        assertEquals(0, sharedState.getMembersOrder().size());
+        assertEquals(0, sharedState.getMembersMap().size());
     }
 
     @Test
     void removeMember() {
+        // Firstly add some members
+        sharedState.initiateMember(connectionHandler1);
+        sharedState.initiateMember(connectionHandler2);
+        sharedState.addMember("123ab:oskar", thread1, "127.0.0.1");
+        sharedState.addMember("124ab:pedro", thread2, "127.0.0.1");
+
+        HashMap<Long, Member> membersMap = sharedState.getMembersMap();
+        LinkedList<String> membersOrder = sharedState.getMembersOrder();
+
+        assertEquals(2, membersMap.size());
+        assertEquals(2, membersOrder.size());
+
+        assertFalse(membersMap.get(thread2).getCoordinator());
+
+        sharedState.removeMember(thread1);
+
+        membersMap = sharedState.getMembersMap();
+        membersOrder = sharedState.getMembersOrder();
+
+        assertEquals(1, membersMap.size());
+        assertEquals(1, membersOrder.size());
+
+        assertTrue(membersMap.get(thread2).getCoordinator());
     }
 }
